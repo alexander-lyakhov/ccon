d426006fe178574e92e77c7f0447f33eec535c78
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-07-31 16:34:01 +0300

    ...

--- _h_/wcharfix.h
+++ _h_/wcharfix.h
@@ -6,7 +6,7 @@
 
 /*
 
-	HOW TO USE:
+HOW TO USE:
 
 	In *.c file define macros WCHAR_FIX_IMPLEMENTATION and include "wcharfix.h"
 
@@ -17,15 +17,34 @@
 		
 		WCHAR_FIX;
 
+	Or just use the function:
+		
+		wprint(const wchar_t* text)
+
 */
 
 #ifndef _WCHARFIX_H_
 #define _WCHARFIX_H_
+
+void wprint(const wchar_t* text);
 
 #ifdef WCHAR_FIX_IMPLEMENTATION
 	#include <fcntl.h>
 	#include <io.h>
 
 	#define WCHAR_FIX _setmode(_fileno(stdout), _O_U16TEXT);
+
+	void wprint(const wchar_t* text)
+	{
+		DWORD written;
+
+		WriteConsoleW(
+			GetStdHandle(STD_OUTPUT_HANDLE),
+			text,
+			lstrlenW(text),
+			&written,
+			NULL
+		);
+	}
 #endif
 #endif
